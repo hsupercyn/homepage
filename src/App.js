25364b0901cf2e04abef8b3f5f6ffc15e473b071
Cynthia Hsu <cynthiaxhsu@berkeley.edu>
2019-10-05 13:51:17 -0500

    add in minimal profile

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+import { Layout, Header, Navigation, Drawer, Content, HeaderRow, HeaderTabs, Tab } from 'react-mdl';
+import Main from './components/main.js';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="demo-big-content">
+      <Layout>
+          <Header title="*this site is in active development" scroll>
+              <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/photo">Photography</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+          </Header>
+          <Drawer title="Title">
+              <Navigation>
+                  <Link to="/resume">Resume</Link>
+                  <Link to="/projects">Projects</Link>
+                  <Link to="/photo">Photography</Link>
+                  <Link to="/contact">Contact</Link>
+              </Navigation>
+          </Drawer>
+          <Content>
+              <div className="page-content" />
+              <Main />
+          </Content>
+      </Layout>
+  </div>
   );
 }
 
